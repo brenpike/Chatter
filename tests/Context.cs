b0a62126e342688676c04e40990e094f24549e53
Brennan Pike <Brennan.Pike@mnp.ca>
2020-09-25 13:04:27 -0600

    Add testing projects

---
+++ tests/Context.cs
@@ -0,0 +1,10 @@
+﻿namespace Chatter.Testing.Core
+{
+    /// <summary>
+    /// This is a base test class providing the ability via a context to create test objects within other test objects
+    /// </summary>
+    public abstract class Context
+    {
+        protected INewContext New { get; }
+    }
+}
