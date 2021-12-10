5186e8c825328a0b1cdf9168a2731e84c4df69e1
Brennan Pike <brenpike@gmail.com>
2021-12-09 22:46:39 -0700

    add DefaultAzureCredential support for development scenarios

--- src/Chatter.MessageBrokers.AzureServiceBus.Auth/src/Chatter.MessageBrokers.AzureServiceBus.Auth/AadTokenProviderFactory.cs
+++ src/Chatter.MessageBrokers.AzureServiceBus.Auth/src/Chatter.MessageBrokers.AzureServiceBus.Auth/AadTokenProviderFactory.cs
@@ -1,93 +1,116 @@
-﻿using Microsoft.Azure.ServiceBus.Primitives;
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.ServiceBus.Primitives;
 using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace Chatter.MessageBrokers.AzureServiceBus.Auth
 {
     public class AadTokenProviderFactory
     {
-        private const string AadServiceBusAudience = "https://servicebus.azure.net/";
         private readonly string _clientId;
-        private readonly Uri _serviceBusAudience;
+        private readonly TokenRequestContext _requestContext;
 
         public static AadTokenProviderFactory Create(string clientId) => new AadTokenProviderFactory(clientId);
 
         private AadTokenProviderFactory(string clientId)
         {
             _clientId = clientId;
-            _serviceBusAudience = new Uri(AadServiceBusAudience);
+            _requestContext = new TokenRequestContext(new[] { "https://servicebus.azure.net/.default" });
         }
 
         /// <summary>
-        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using a client secret
+        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using a client secret. If no client secret is provided <see cref="DefaultAzureCredential"/> is used.
         /// </summary>
         /// <param name="clientSecret">The client secret to use to authenticate with Azure AD</param>
         /// <param name="authority">A URL that indicates a directory that MSAL can request tokens from. For example, https://login.microsoftonline.com/{AzureADTenantID}/</param>
-        /// <param name="state">Custom state to provide the auth callback</param>
         /// <returns><see cref="AzureActiveDirectoryTokenProvider"/></returns>
-        public AzureActiveDirectoryTokenProvider WithSecret(string clientSecret, string authority, object state = null)
+        public AzureActiveDirectoryTokenProvider WithSecret(string clientSecret, string authority, Action<DefaultAzureCredentialOptions> optBuilder = null)
         {
             AzureActiveDirectoryTokenProvider.AuthenticationCallback authCallback = async (audience, authority, state) =>
             {
+                if (string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    return (await GetTokenFromDefaultAzureCredential(optBuilder)).Token;
+                }
+
                 IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(_clientId)
                                 .WithAuthority(authority)
                                 .WithClientSecret(clientSecret)
                                 .Build();
 
-                var authResult = await app.AcquireTokenForClient(new string[] { $"{_serviceBusAudience}/.default" }).ExecuteAsync();
+                var authResult = await app.AcquireTokenForClient(_requestContext.Scopes).ExecuteAsync();
 
                 return authResult.AccessToken;
             };
 
-            return new AzureActiveDirectoryTokenProvider(authCallback, authority, state);
+            return new AzureActiveDirectoryTokenProvider(authCallback, authority ?? "", null);
         }
 
         /// <summary>
-        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using a certificate
+        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using a certificate. If no thumbprint is provided <see cref="DefaultAzureCredential"/> is used.
         /// </summary>
         /// <param name="thumbPrint">The thumbprint of the certificate to use for authentication</param>
         /// <param name="authority">A URL that indicates a directory that MSAL can request tokens from. For example, https://login.microsoftonline.com/{AzureADTenantID}/
         /// <param name="validCertsOnly">Indicates if only valid certificates are can be found and used from the X509 cert store. If using self-signed certs, this value should be false.</param>
-        /// <param name="state">Custom state to provide the auth callback</param>
         /// <returns><see cref="AzureActiveDirectoryTokenProvider"/></returns>
-        public AzureActiveDirectoryTokenProvider WithCert(string thumbPrint, string authority, bool validCertsOnly, object state = null)
+        public AzureActiveDirectoryTokenProvider WithCert(string thumbPrint, string authority, bool validCertsOnly, Action<DefaultAzureCredentialOptions> optBuilder = null)
         {
-            var cert = GetCertificate(thumbPrint, validCertsOnly);
             AzureActiveDirectoryTokenProvider.AuthenticationCallback authCallback = async (audience, authority, state) =>
             {
+                if (string.IsNullOrWhiteSpace(thumbPrint))
+                {
+                    return (await GetTokenFromDefaultAzureCredential(optBuilder)).Token;
+                }
+
+                var cert = GetCertificate(thumbPrint, validCertsOnly);
+
                 IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(_clientId)
                                 .WithAuthority(authority)
                                 .WithCertificate(cert)
                                 .Build();
 
-                var authResult = await app.AcquireTokenForClient(new string[] { $"{_serviceBusAudience}/.default" }).ExecuteAsync();
+                var authResult = await app.AcquireTokenForClient(_requestContext.Scopes).ExecuteAsync();
                 return authResult.AccessToken;
             };
 
-            return new AzureActiveDirectoryTokenProvider(authCallback, authority, state);
+            return new AzureActiveDirectoryTokenProvider(authCallback, authority ?? "", null);
         }
 
         /// <summary>
-        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using interactive login
+        /// Creates an <see cref="AzureActiveDirectoryTokenProvider"/> using interactive login. If no redirect url is provided <see cref="DefaultAzureCredential"/> is used.
         /// </summary>
         /// <param name="redirectUri">The uri to redirect to after interactive login</param>
-        /// <param name="state">Custom state to provide the auth callback</param>
         /// <returns><see cref="AzureActiveDirectoryTokenProvider"/></returns>
-        public AzureActiveDirectoryTokenProvider WithInteractive(string redirectUri, object state = null)
+        public AzureActiveDirectoryTokenProvider WithInteractive(string redirectUri, Action<DefaultAzureCredentialOptions> optBuilder = null)
         {
             AzureActiveDirectoryTokenProvider.AuthenticationCallback authCallback = async (audience, authority, state) =>
             {
+                if (string.IsNullOrWhiteSpace(redirectUri))
+                {
+                    return (await GetTokenFromDefaultAzureCredential(optBuilder)).Token;
+                }
+
                 IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(_clientId)
                                 .WithRedirectUri(redirectUri)
                                 .Build();
 
-                var authResult = await app.AcquireTokenForClient(new string[] { $"{_serviceBusAudience}/.default" }).ExecuteAsync();
+                var authResult = await app.AcquireTokenForClient(_requestContext.Scopes).ExecuteAsync();
                 return authResult.AccessToken;
             };
 
-            return new AzureActiveDirectoryTokenProvider(authCallback, "", state);
+            return new AzureActiveDirectoryTokenProvider(authCallback, "", null);
+        }
+
+        private ValueTask<AccessToken> GetTokenFromDefaultAzureCredential(Action<DefaultAzureCredentialOptions> optBuilder)
+        {
+            var opts = new DefaultAzureCredentialOptions();
+            optBuilder?.Invoke(opts);
+            var defaultCredentials = new DefaultAzureCredential(opts);
+            return defaultCredentials.GetTokenAsync(_requestContext, System.Threading.CancellationToken.None);
         }
 
         X509Certificate2 GetCertificate(string thumbPrint, bool validCertsOnly)
--- src/Chatter.MessageBrokers.AzureServiceBus.Auth/src/Chatter.MessageBrokers.AzureServiceBus.Auth/ServiceBusOptionsBuilderExtensions.cs
+++ src/Chatter.MessageBrokers.AzureServiceBus.Auth/src/Chatter.MessageBrokers.AzureServiceBus.Auth/ServiceBusOptionsBuilderExtensions.cs
@@ -1,25 +1,55 @@
-﻿using Chatter.MessageBrokers.AzureServiceBus.Auth;
+﻿using Azure.Identity;
+using Chatter.MessageBrokers.AzureServiceBus.Auth;
 using Chatter.MessageBrokers.AzureServiceBus.Options;
+using System;
+using Microsoft.Azure.ServiceBus.Primitives;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceBusOptionsBuilderExtensions
     {
-        public static ServiceBusOptionsBuilder UseAadTokenProviderWithSecret(this ServiceBusOptionsBuilder builder, string clientId, string clientSecret, string authority, object state = null)
+        /// <summary>
+        /// Uses an <see cref="AzureActiveDirectoryTokenProvider"/> for Azure Service Bus authentication via a client secret. If no client secret is provided <see cref="DefaultAzureCredential"/> is used.
+        /// </summary>
+        /// <param name="builder">The <see cref="ServiceBusOptionsBuilder"/> used to configure Azure Service Bus authentication</param>
+        /// <param name="clientId">The client ID of the service principal</param>
+        /// <param name="clientSecret">The client secret to use to authenticate with Azure AD</param>
+        /// <param name="authority">A URL that indicates a directory that MSAL can request tokens from. For example, https://login.microsoftonline.com/{AzureADTenantID}/</param>
+        /// <param name="optBuilder">An optional builder to construct <see cref="DefaultAzureCredentialOptions"/> to be used with <see cref="DefaultAzureCredential"/> when no client secret is provided.</param>
+        /// <returns>a <see cref="ServiceBusOptionsBuilder"/></returns>
+        public static ServiceBusOptionsBuilder UseAadTokenProviderWithSecret(this ServiceBusOptionsBuilder builder, string clientId, string clientSecret, string authority, Action<DefaultAzureCredentialOptions> optBuilder = null)
         {
-            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithSecret(clientSecret, authority, state));
+            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithSecret(clientSecret, authority));
             return builder;
         }
 
-        public static ServiceBusOptionsBuilder UseAadTokenProviderWithCert(this ServiceBusOptionsBuilder builder, string clientId, string thumbPrint, string authority, bool validCertsOnly = true, object state = null)
+        /// <summary>
+        /// Uses an <see cref="AzureActiveDirectoryTokenProvider"/> for Azure Service Bus authentication via a client certificate. If no thumbprint is provided <see cref="DefaultAzureCredential"/> is used.
+        /// </summary>
+        /// <param name="builder">The <see cref="ServiceBusOptionsBuilder"/> used to configure Azure Service Bus authentication</param>
+        /// <param name="clientId">The client ID of the service principal</param>
+        /// <param name="thumbPrint">The thumbprint of the certificate to be used for authentication</param>
+        /// <param name="authority">A URL that indicates a directory that MSAL can request tokens from. For example, https://login.microsoftonline.com/{AzureADTenantID}/</param>
+        /// <param name="optBuilder">An optional builder to construct <see cref="DefaultAzureCredentialOptions"/> to be used with <see cref="DefaultAzureCredential"/> when no thumbprint is provided.</param>
+        /// <param name="validCertsOnly"></param>
+        /// <returns>a <see cref="ServiceBusOptionsBuilder"/></returns>
+        public static ServiceBusOptionsBuilder UseAadTokenProviderWithCert(this ServiceBusOptionsBuilder builder, string clientId, string thumbPrint, string authority, Action<DefaultAzureCredentialOptions> optBuilder = null, bool validCertsOnly = true)
         {
-            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithCert(thumbPrint, authority, validCertsOnly, state));
+            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithCert(thumbPrint, authority, validCertsOnly));
             return builder;
         }
 
-        public static ServiceBusOptionsBuilder UseAadTokenProviderInteractively(this ServiceBusOptionsBuilder builder, string clientId, string redirectUri, object state = null)
+        /// <summary>
+        /// Uses an <see cref="AzureActiveDirectoryTokenProvider"/> for Azure Service Bus authentication via a client certificate. If no redirect url is provided <see cref="DefaultAzureCredential"/> is used.
+        /// </summary>
+        /// <param name="builder">The <see cref="ServiceBusOptionsBuilder"/> used to configure Azure Service Bus authentication</param>
+        /// <param name="clientId">The client ID of the service principal</param>
+        /// <param name="redirectUri">The uri to redirect to after successful interactive login</param>
+        /// <param name="optBuilder">A builder to construct <see cref="DefaultAzureCredentialOptions"/> to be used with <see cref="DefaultAzureCredential"/> when no redirect url is provided.</param>
+        /// <returns>a <see cref="ServiceBusOptionsBuilder"/></returns>
+        public static ServiceBusOptionsBuilder UseAadTokenProviderInteractively(this ServiceBusOptionsBuilder builder, string clientId, string redirectUri, Action<DefaultAzureCredentialOptions> optBuilder = null)
         {
-            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithInteractive(redirectUri, state));
+            builder.AddTokenProvider(() => AadTokenProviderFactory.Create(clientId).WithInteractive(redirectUri));
             return builder;
         }
     }
